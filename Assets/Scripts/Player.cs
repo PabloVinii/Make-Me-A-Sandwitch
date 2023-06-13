74fde374b83ec81832126eaab004fcd07c323f15
PabloVinii <pvinicius006@hotmail.com>
2023-06-13 16:14:25 -0300

    collision detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,10 +14,52 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerheight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerheight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            //attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;    
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerheight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                //can move only on the X
+                moveDir = moveDirX;
+            } 
+            else
+            {
+                // cannot move only on the X
+
+                //attempt only Z movement    
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;  
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerheight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    // can move only on the Z
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    //cannot move in any direction
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+        
+
         isWalking = moveDir != Vector3.zero;
 
         float rotateSpeed = 12f;
