313293ecf32bfae36a1d8bb1c1e4f2f321570f8a
PabloVinii <pvinicius006@hotmail.com>
2023-06-15 15:35:09 -0300

    plate world canvas

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -36,6 +36,10 @@ public class PlateKitchenObject : KitchenObject
             OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs{kitchenObjectSO = kitchenObjectSO});
             return true;
         }
-        
+    }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
     }
 }
