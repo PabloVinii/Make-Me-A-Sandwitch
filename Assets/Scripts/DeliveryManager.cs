f3a9b8f5e6de1d3ca34ae05b9befa47e8fefb889
PabloVinii <pvinicius006@hotmail.com>
2023-06-16 00:59:05 -0300

    Sound Effects

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -7,6 +7,8 @@ public class DeliveryManager : MonoBehaviour
 {
     public event EventHandler OnRecipeSpawned; 
     public event EventHandler OnRecipeCompleted; 
+    public event EventHandler OnRecipeSuccess; 
+    public event EventHandler OnRecipeFailed; 
 
     public static DeliveryManager Instance{get; private set;} 
     [SerializeField] private RecipeListSO recipeListSO; 
@@ -69,15 +71,15 @@ public class DeliveryManager : MonoBehaviour
                 }
                 if (plateContentsMatchesRecipe)
                 {
-                    Debug.Log("correto");
                     waitingRecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+                    OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
                     return;
                 }
             }
         }
-        Debug.Log("errado");
+        OnRecipeFailed?.Invoke(this, EventArgs.Empty);
     }
 
     public List<RecipeSO> GetWaitingRecipeSOList()
