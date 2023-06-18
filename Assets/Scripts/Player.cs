baf890ad34b0aa9c9471e5c9407027c6b96a147c
PabloVinii <pvinicius006@hotmail.com>
2023-06-18 14:26:56 -0300

    level build

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -117,7 +117,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
         {
             //attempt only X movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;    
-            canMove = moveDir.x != 0 &&  !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerheight, playerRadius, moveDirX, moveDistance);
+            canMove = (moveDir.x < -.5f || moveDir.x > + .5f) &&  !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerheight, playerRadius, moveDirX, moveDistance);
 
             if (canMove)
             {
@@ -130,7 +130,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
                 //attempt only Z movement    
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;  
-                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerheight, playerRadius, moveDirZ, moveDistance);
+                canMove = (moveDir.z < -.5f || moveDir.z > + .5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerheight, playerRadius, moveDirZ, moveDistance);
 
                 if (canMove)
                 {
